3d9547cbd9b338b4ff9a110e127050068d508d21
Tuhin Gupta <tuhin.gupta@aexp.com>
2016-01-26 20:07:32 -0700

    assignment 2 scope and forms

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -78,7 +78,7 @@ angular.module('confusionApp', [])
        .controller('ContactController', ['$scope', function($scope) {
             $scope.channelmsg = "Select a Channel";
             $scope.invalidChannelSelection = false;
-            
+
             $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
                         var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
                         $scope.channels = channels;
@@ -113,6 +113,72 @@ angular.module('confusionApp', [])
                         }
                   }
             };
+        }])
+
+        .controller('DishDetailController',['$scope', function($scope) {
+
+            var dish={
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains', 
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comments: [
+                               {
+                                   rating:5,
+                                   comment:"Imagine all the eatables, living in conFusion!",
+                                   author:"John Lemon",
+                                   date:"2012-10-16T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                   author:"Paul McVites",
+                                   date:"2014-09-05T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:3,
+                                   comment:"Eat it, just eat it!",
+                                   author:"Michael Jaikishan",
+                                   date:"2015-02-13T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Ultimate, Reaching for the stars!",
+                                   author:"Ringo Starry",
+                                   date:"2013-12-02T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:2,
+                                   comment:"It's your birthday, we're gonna party!",
+                                   author:"25 Cent",
+                                   date:"2011-12-02T17:57:28.556094Z"
+                               }
+                               
+                           ]
+                    };
+            
+            $scope.dish = dish;
+            $scope.predicate = '';
+
+            $scope.inputComment = { author:"", date:"", rating:5, comment:"" };
+
+            $scope.submitComment = function(){
+
+              $scope.inputComment.date = new Date();
+              $scope.inputComment.rating = Number($scope.inputComment.rating);
+              console.log($scope.inputComment);
+
+              $scope.dish.comments.push($scope.inputComment);
+              console.log($scope.dish);
+
+              $scope.inputComment = { author:"", date:"", rating:5, comment:"" };
+              $scope.commentsForm.$setPristine();
+
+            };
+                       
+            
         }]) 
 
        
